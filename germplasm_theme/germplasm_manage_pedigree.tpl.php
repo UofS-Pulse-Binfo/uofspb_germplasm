cf0a0bb0c6dc2d07cc4cd5060eec0692a2abe4fb
Lacey Sanderson <laceysanderson@781094.no-reply.drupal.org>
2013-04-10 13:53:31 -0600

    Last cleanup before release of new Tripal 1.0 compatible version :)

--- germplasm_theme/germplasm_manage_pedigree.tpl.php
+++ germplasm_theme/germplasm_manage_pedigree.tpl.php
@@ -1,7 +1,7 @@
-<div id="tripal_stock-germplasm_pedigree-box" class="germplasm_manage-info-box tripal-info-box"> 
-  <div class="germplasm_manage-info-box-title tripal-info-box-title">Pedigree</div> 
+<div id="tripal_stock-germplasm_pedigree-box" class="germplasm_manage-info-box tripal-info-box">
+  <div class="germplasm_manage-info-box-title tripal-info-box-title">Pedigree</div>
 
-  <div class="views-exposed-widget"> 
+  <div class="views-exposed-widget">
     <?php
       print drupal_get_form('germplasm_manage_generate_pedigree_on_node_form', $node->stock->stock_id);
     ?>
@@ -17,18 +17,19 @@
       $pedigree = germplasm_manage_generate_subject_pedigree_array($node->stock->stock_id, $num_levels);
       $pedigree_string = germplasm_manage_generate_textual_pedigree_string($pedigree, $num_levels);
     }
-    
+
   ?>
   <p><th>The pedigree that is depicted below is a representation of the lineage or genealogical descent of the selected stock.
   The lineage has been traced to <?php print $_GET['generation']; ?> generations. </th></p><br>
 
-  <table class="tripal_germplasm_manage-table tripal-table tripal-table-horz">
-    <tr><th>Pedigree</th></tr>      
+  <p><strong>Pedigree: </strong>
     <?php if (!empty($pedigree_string)) { ?>
-      <tr><td class="tripal_germplasm_manage-table-even-row tripal-table-even-row"><?php print $pedigree_string; ?></td></tr>
+      <?php print $pedigree_string; ?></p>
+    <?php } else { ?>
+      Unknown</p>
     <?php } ?>
   </table>
-  <?php print theme('germplasm_manage_pedigree_drawing', $pedigree); ?>
-</div> 
+  <?php //print theme('germplasm_manage_pedigree_drawing', $pedigree); ?>
+</div>
 
 
