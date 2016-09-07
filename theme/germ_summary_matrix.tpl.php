5cb7b54350b0c52d776e89caa92ab19488db4915
Reynold Tan <reynoldltan@gmail.com>
2016-09-07 14:04:42 -0600

    Initial commit of germplasm summary matrix.

---
+++ theme/germ_summary_matrix.tpl.php
@@ -0,0 +1,55 @@
+<?php
+  /**
+   * @file
+   * Master template file of Wild Germ Summary Module.
+   *
+   * Available variables:
+   * -
+   */
+  
+   // Template structure:
+   // <div>           - Main div container
+   //   <div>         - div container for matrix 
+   //     <div></div> - Maternal Parent caption
+   //     Matrix      - The matrix
+   //   </div>
+   //
+   //   <div>         - Container for summary table
+   //     <div></div> - div container for summary table title
+   //     <div></div> - Horizontal line
+   //     Table       - The table
+   //   </div>
+   // </div>
+  
+  // Determine if table is present do not show the matrix. 
+  $display_matrix = '';
+  if (isset($form['siblings_mom']['#value']) AND isset($form['siblings_dad']['#value'])) {
+    $display_matrix = 'no-display';
+  }
+?>
+
+<div id="container-summary">
+  
+  <div id="container-matrix" class="<?php print $display_matrix; ?>">
+    <div class="table-caption horizontal-col">&nbsp;</div>
+    <?php print drupal_render($form['summary_matrix']); ?>
+  </div>
+  
+  <?php if ($display_matrix == 'no-display') { ?>
+    <div id="container-table">
+      <div>
+        <ul id="table-title-cells">
+          <li><?php print $form['siblings_mom']['#value']; ?><small>Maternal Parent</small></li>
+          <li><?php print $form['siblings_dad']['#value']; ?><small>Paternal Parent</small></li>
+          <li><?php print $form['siblings_count']['#value']; ?> Stock Records<small>&nbsp;</small></li>
+        </ul>
+    
+        <div class="nav-link"><a href="#">Wild Lentil Crosses Matrix</a></div>
+      </div>
+    
+      <div class="div-line">&nbsp;</div>
+      <?php print drupal_render($form['summary_table']); ?>
+    </div> 
+  <?php } ?>
+  
+</div>
---
+++ theme/js/germ_summary.script.js
@@ -0,0 +1,79 @@
+/**
+ * @file 
+ * Handle events in summary matrix
+ */
+ 
+(function($) {
+  Drupal.behaviors.summaryMatrixBehaviour = {
+    attach: function (context, settings) {
+      // Add event listener to link to show matrix.
+      if ($('.nav-link').length) {
+        $('.nav-link').click(function(e) {
+          e.preventDefault(); 
+          // Destroy table from dom and repost matrix.
+          $('#container-table').remove();
+          $('#container-matrix').removeClass('no-display');
+        });
+      }
+      
+      // Add event listener to table cells.
+      $('td')
+      ////
+      .mouseover(function() {
+        // child object.
+        var siblings = {};
+        
+        // Get the id attribute of the selected cell.
+        // The id attribute contains parent cell index in table element.
+        // eg. p_2_3 where the first digit is the mother and the second
+        //     digit is the father.
+        siblings.attrId = $(this).attr('id');
+        var parents = siblings.attrId.split('-');
+        // Get the index of parents;
+        siblings.mom = parents[1];
+        siblings.dad = parents[2];
+
+        // Highlight sibling count cell and parents as well.
+        highlightParentCells(siblings); 
+      })
+      ////
+      .mouseout(function() {
+        // Remove all highlights to cell, parents and between.
+        highlightParentCells(0); 
+      });
+      
+      // Function to highlight parents of siblings count.
+      function highlightParentCells(siblings) {
+        if (siblings) {
+          // Style the child cell first.
+          var theCell = $('#' + siblings.attrId);
+          theCell.addClass('highlight-child');
+          
+          // Style the mom and dad cell.
+          var momTh = $('#maternal-' + siblings.mom);
+          momTh.addClass('highlight-mom');
+      
+          var dadTh = $('#paternal-' + siblings.dad);
+          dadTh.addClass('highlight-dad');   
+          
+          // Create a beam effect from child cell to mom and dad cell.
+          for(var k = siblings.dad - 1; k > 0; k--) {
+            // Move one cell up.
+            td = $('#paternal-' + k).siblings('td');
+            $(td[siblings.mom - 1]).addClass('highlight-dad-cells');
+          }
+
+          var td = dadTh.siblings('td');
+          for(var i = 0; i < siblings.mom; i++) {
+            // Move one cell to the right.
+            $(td[i - 1]).addClass('highlight-mom-cells');
+          }
+        }
+        else {
+          // Remove all highlighting classes attached to table headers and cells.
+          $('th, td').removeClass();
+        }
+      }
+    }
+  };
+}(jQuery));
