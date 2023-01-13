92abfc3d86c0259225b8dd0850917a3bf8ab7713
reynold tan <reynoldltan@gmail.com>
2023-01-13 14:53:45 -0600

    Adds search stock field in a collection

--- germcollection/theme/script/script-collection-view.js
+++ germcollection/theme/script/script-collection-view.js
@@ -5,10 +5,23 @@
 (function ($) {
   Drupal.behaviors.scriptCollectionView = {
     attach: function (context, settings) {   
-       
     // Search box  
     var fldNameSearch = 'germcollection-fld-textfield-search';
     var fldSearch = $('#' + fldNameSearch);
+    // Host.
+    var host = Drupal.settings.germcollection.host;  
+    
+    // Select text value of germplasm field when clicked
+    // to assist quick modification/replacement of search text.
+    fldSearch.click(function(e) {
+      $(this).select();
+    });
+    
+    // Autocomplete search germplasm in a collection.
+    fldSearch
+      .addClass('germcollection-autocomplete-inactive')
+      .autocomplete({
+        
+      });
 
-   
-}};}(jQuery));
+  }};}(jQuery));
